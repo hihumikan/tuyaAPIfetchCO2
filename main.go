83be1a512bfc48f61ef3edf9f85eeb473ea51e00
hihumikan <a@qqey.net>
2023-09-28 22:13:36 +0900

    feat: accesstokenresponce types

--- main.go
+++ main.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 )
+
+type AccessTokenResponse struct {
+	Result struct {
+		AccessToken  string `json:"access_token"`
+		ExpireTime   int    `json:"expire_time"`
+		RefreshToken string `json:"refresh_token"`
+		UID          string `json:"uid"`
+	} `json:"result"`
+	Success bool   `json:"success"`
+	T       int    `json:"t"`
+	TID     string `json:"tid"`
+}
 
 func main() {
 
@@ -37,5 +50,14 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	fmt.Println(string(body))
+
+	var response AccessTokenResponse
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	accessToken := response.Result.AccessToken
+	fmt.Println("アクセストークン:", accessToken)
 }
